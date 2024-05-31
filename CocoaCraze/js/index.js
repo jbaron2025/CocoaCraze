5e9a63aee901cbe9de1beaafca83f83a057205cd
jbaron2025 <121137563+jbaron2025@users.noreply.github.com>
2024-05-31 11:16:18 -0700

    Add files via upload

---
+++ CocoaCraze/js/index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  centeredSlides: true, loop: true,
+  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
