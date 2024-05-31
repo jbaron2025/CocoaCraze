d25ab5cb8f935447b60bddf1e20c1770de1f78bf
jbaron2025 <121137563+jbaron2025@users.noreply.github.com>
2024-05-31 11:17:21 -0700

    Delete CocoaCraze directory

--- CocoaCraze/js/index.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".home-swiper", {
-  spaceBetween: 30,
-  centeredSlides: true, loop: true,
-  
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
